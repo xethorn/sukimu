40a4ab240ddf8a7a0d6a6fd24deb1c01ec8c658f
Michael Ortali <github@xethorn.net>
2016-11-12 18:32:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='Sukimu',
-    version='2.0.4',
+    version='2.0.5',
     url='https://github.com/xethorn/sukimu',
     author='Michael Ortali',
     author_email='github@xethorn.net',
